de698b6891ca57670d45cc43892eb4219a37c3bf
ninetag <lureloa96@gmail.com>
2024-02-14 11:26:50 +0900

    first commit

---
+++ src/main/java/e6eo/finalproject/member/MemberDAO.java
@@ -0,0 +1,11 @@
+package e6eo.finalproject.member;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberDAO {
+
+    public boolean loginChk() {
+
+    }
+}
---
+++ src/test/java/e6eo/finalproject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package e6eo.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
