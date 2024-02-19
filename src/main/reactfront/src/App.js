a24b477e38708a1899479c4974a41522f0ea54fc
ninetag <lureloa96@gmail.com>
2024-02-19 17:33:37 +0900

    스프링부트 & 리액트 연동 Backup Commit (0219)

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -1,35 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
+import logo from './temp_logo.png'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
-  // 요청받은 정보를 담아줄 변수 선언
-  const [ testStr, setTestStr ] = useState('');
 
-  // 변수 초기화
-  function callback(str) {
-    setTestStr(str);
-  }
-
-  // 첫 번째 렌더링을 마친 후 실행
-  useEffect(
-      () => {
-        axios({
-          url: '/home',
-          method: 'GET'
-        }).then((res) => {
-          callback(res.data);
-        })
-      }, []
-  );
-
-  return (
-      <div className="App">
-        <header className="App-header">
-          {testStr}
-        </header>
-      </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App;
