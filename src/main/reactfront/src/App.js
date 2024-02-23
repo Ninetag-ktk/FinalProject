70acdb8fe7531b8d613a5c17c59e2372818eda61
seorema <artery2004@naver.com>
2024-02-23 12:40:00 +0900

    조장님이 수정하신 버전

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -11,11 +11,41 @@ import {
 
 function App() {
 
+    // message 초기값 설정 (""로 설정)
+    const [message, setMessage] = useState("");
+    const [reResponse, setResResponse] = useState();
+
+    // useEffect(함수, 배열) : 컴포넌트가 화면에 나타났을 때 자동 실행
+    useEffect(() => {
+        // fetch(url, options) : Http 요청 함수
+        fetch("/google/request")
+            .then(response => response.text())
+            .then(authorizationUrl => {
+                // 1. 새 창에서 리다이렉션
+                window.open(authorizationUrl, "_blank");
+                //
+                // 2. 리다이렉션 URL을 상태 변수에 저장
+                //    (예: `setMessage(authorizationUrl)`)
+                //
+                // 3. 컨트롤러 또는 모델로 데이터 전송
+                //    (예: `fetch("/api/callback", {
+                //        method: "POST",
+                //        body: JSON.stringify({ authorizationUrl }),
+                //    })`)
+            })
+    ;
+    }, [])
+
     return (
-        <div>
-            <h1>hello</h1>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    google/request : {message}
+                </p>
+            </header>
         </div>
-    )
+    );
 }
 
 export default App;
