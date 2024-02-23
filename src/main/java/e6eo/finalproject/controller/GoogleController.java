3fd42692895c36b082008ececcce6a0da14c3816
seorema <artery2004@naver.com>
2024-02-23 12:40:00 +0900

    조장님이 수정하신 버전

--- src/main/java/e6eo/finalproject/controller/GoogleController.java
+++ src/main/java/e6eo/finalproject/controller/GoogleController.java
@@ -4,6 +4,7 @@ import e6eo.finalproject.dao.GoogleDAO;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,22 @@ public class GoogleController {
     @Autowired
     private GoogleDAO gDao;
 
-    @GetMapping("/request")
-    public String googleUser() throws GeneralSecurityException, IOException {
-        gDao.googleGet();
+    @GetMapping("")
+    public String aaa(){
+        return "집가고싶다";
+    }
 
-        return null;
+
+    @GetMapping("/request")
+    public String googleUser() {
+        String re = null;
+        try {
+            re = gDao.getAuthorization();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return re;
     }
 
 
--- src/main/java/e6eo/finalproject/controller/UserController.java
+++ src/main/java/e6eo/finalproject/controller/UserController.java
@@ -3,21 +3,23 @@ package e6eo.finalproject.controller;
 import e6eo.finalproject.dao.UsersDAO;
 import e6eo.finalproject.entity.Users;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
+
     @Autowired
     private UsersDAO uDao;
 
-
     @PostMapping("/join")
     public String userJoin(@RequestBody Users users){
-        uDao.userJoin(users);
+            uDao.userJoin(users);
+
         return null;
     }
 }
