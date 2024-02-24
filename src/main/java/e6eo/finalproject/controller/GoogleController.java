5f8d5da0e6c4def2bb3e401f2526c1e7ca20bbbd
Ninetag-ktk <lureloa96@gmail.com>
2024-02-25 08:17:01 +0900

    브랜치 업데이트 (from develop, 0228)

--- src/main/java/e6eo/finalproject/controller/GoogleController.java
+++ src/main/java/e6eo/finalproject/controller/GoogleController.java
@@ -1,10 +1,7 @@
 package e6eo.finalproject.controller;
 
 import e6eo.finalproject.dao.GoogleDAO;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +14,7 @@ public class GoogleController {
     private GoogleDAO gDao;
 
     @GetMapping("")
-    public String aaa(){
+    public String aaa() {
         return "집가고싶다";
     }
 
