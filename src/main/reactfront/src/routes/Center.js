88f738ffae9d622fb474603c4e0cefa281b2cdd4
chanwoo kang <rkd555777@gmail.com>
2024-02-26 10:38:58 +0900

    Center.js

--- src/main/reactfront/src/routes/Center.js
+++ src/main/reactfront/src/routes/Center.js
@@ -13,7 +13,61 @@ import {
 
 export default function center () {
     return (
-        <h1>Center  입니다.</h1>
+        <table border={1}>
+            <th>여기에 캘린더 나옴</th>
+            <th>여기에 캘린더 나옴2</th>
+            <tr>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+            </tr>
+            <tr>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+            </tr>
+            <tr>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+            </tr>
+            <tr>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+            </tr>
+            <tr>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+                <td>dd</td>
+            </tr>
+
+        </table>
     )
 }
 
