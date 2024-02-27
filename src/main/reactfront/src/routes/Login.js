ea7dd41a415cf627228365d7c988c7f0c95e7f82
seorema <artery2004@naver.com>
2024-02-27 15:26:31 +0900

    react에서 데이터 받아오는 작업중

--- src/main/reactfront/src/routes/Login.js
+++ src/main/reactfront/src/routes/Login.js
@@ -14,9 +14,9 @@ export default function Login ()  {
             </div>
 
             <div className={"logininput"}>
-                <form className={"loginForm1"} action={"/main"}>
-                    <input type={"text"} className={"inputtext"} placeholder={"email"}/>
-                    <input type={"password"} className={"inputtext"} placeholder={"password"} />
+                <form className={"loginForm1"} action={"/main"} onSubmit={fetch("/main")}>
+                    <input type={"text"} className={"inputtext"} name={"id"} placeholder={"email"}/>
+                    <input type={"password"} className={"inputtext"} name={"pw"} placeholder={"password"} />
                     <button id={"btn"}>로그인</button>
 
                 </form >
