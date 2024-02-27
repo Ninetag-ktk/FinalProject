d006b85b925ad8c7f1b8c47d93ec33722a34a69b
chanwoo kang <rkd555777@gmail.com>
2024-02-27 17:26:16 +0900

    main(header,leftbar,center)css

--- src/main/reactfront/src/routes/Header.js
+++ src/main/reactfront/src/routes/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import logo from './nobglogo.png';
 export default function Header () {
     return (
-        <nav>
-            <button>검색</button>
-            <hr/>
-        </nav>
+        <div className={"header"}>
+            <a href={"/main"}> <img className={"logo"} src={logo}/></a>
+                <button id={"btnlastmonth"}>←</button>
+                yyyy.MM
+                <button id={"btnnextmonth"}>→</button>
+                <button id={"menubtn"}>menu</button>
+        </div>
 
-    )
+)
 }
 
