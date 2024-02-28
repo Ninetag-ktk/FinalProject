cac71a9f978c2cdeafb00a8e34d270bdf5fc5bb9
chanwoo kang <rkd555777@gmail.com>
2024-02-28 17:26:17 +0900

    0228 백업푸쉬

--- src/main/reactfront/src/routes/Header.js
+++ src/main/reactfront/src/routes/Header.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React,{ useState, useContext } from "react";
 import logo from './nobglogo.png';
+import Main from "./Main";
+import { MyContext } from "./Main";
 
 
 
+const Page1 = () => {
+    return <h1>페이지 1</h1>;
+};
 
+const Page2 = () => {
+    return <h1>페이지 2</h1>;
+};
 
 
 export default function Header () {
+    const { isMain, handleToggle } = useContext(MyContext);
     return (
         <div className={"header"}>
             <a href={"/main"}> <img className={"logo"} src={logo}/></a>
             <button id={"btnlastmonth"}>←</button>
             yyyy.MM
             <button id={"btnnextmonth"}>→</button>
-
+            <div>
             <label className={"toggleSwitch"}>
-                    <input role={"switch"} type={"checkbox"} />
-                <span  className="toggleButton"></span>
+                    <input type="checkbox" onChange={handleToggle} />
+                <span>캘린더/검색</span>
             </label>
+
+            </div>
         </div>
 
     )
