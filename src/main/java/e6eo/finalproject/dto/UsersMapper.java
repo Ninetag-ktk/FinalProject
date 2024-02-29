079884bbffa22e4042b2a814388612ca21ac9911
ninetag <lureloa96@gmail.com>
2024-02-29 10:15:55 +0900

    백업푸시 (0229)

--- src/main/java/e6eo/finalproject/dto/UsersMapper.java
+++ src/main/java/e6eo/finalproject/dto/UsersMapper.java
@@ -4,6 +4,7 @@ package e6eo.finalproject.dto;
 import e6eo.finalproject.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,14 +13,14 @@ import java.util.Optional;
 @Repository
 public interface UsersMapper extends JpaRepository<UsersEntity, String> {
     @Query(value = "select u from users u where u.innerId=:innerId")
-    public List<UsersEntity> findByInnerId(String innerId);
+    public List<UsersEntity> findByInnerId(@Param("innerId") String innerId);
 
     @Query(value = "select u from users u where u.observeToken=:observeToken")
-    public Optional<UsersEntity> findByObserveToken(String observeToken);
+    public Optional<UsersEntity> findByObserveToken(@Param("observeToken") String observeToken);
 
     @Query(value = "update users u set u.observeToken=:observeToken where u.userId=:userId")
-    public void updateObserveByUserId(String userId, String observeToken);
+    public void updateObserveByUserId(@Param("userId") String userId, @Param("observeToken") String observeToken);
 
     @Query(value = "update users u set u.observeToken=null where u.observeToken=:observeToken")
-    public void emptyObserve(String observeToken);
+    public void emptyObserve(@Param("observeToken") String observeToken);
 }
