dc9f6c8d71ac39ca7d81a339337fb4a4a4c8804c
ninetag <lureloa96@gmail.com>
2024-03-06 11:41:26 +0900

    <베포 푸시 (0306)> - 구글 연동 계정이 로그인 되었을 때 내용 업데이트 메서드 작성 - 풀 캘린더 푸시

--- src/main/java/e6eo/finalproject/controller/GoogleController.java
+++ src/main/java/e6eo/finalproject/controller/GoogleController.java
@@ -60,8 +60,9 @@ public class GoogleController {
         notesDAO.getGoogleNotes(data.get("observe"));
     }
 
-    @PostMapping("/")
+    @PostMapping("/updateCheck")
     public void checkGoogleAccount(@RequestBody Map<String, String> req) {
+        System.out.println("구글 데이터 업데이트 테스트");
         UsersEntity user = usersMapper.findByObserveToken(req.get("observe")).get();
         String accessToken = googleAPI.getNewAccessTokenByObserve(req.get("observe"));
         if (!user.getInnerId().isEmpty()) {
@@ -70,7 +71,7 @@ public class GoogleController {
                 // 데이터를 요청해서 받아옴
                 notesDAO.getGoogleNotes(user, accessToken);
             } else {
-
+                notesDAO.checkGoogleNotes(user, accessToken);
             }
         }
     }
