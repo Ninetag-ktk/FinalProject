4ec49aadb15eb0f8ac4e9de083e329ada05b06d1
seorema <artery2004@naver.com>
2024-03-06 16:09:55 +0900

    년, 월, 옵저버토큰 데이터 보내기

---
+++ src/main/reactfront/src/routes/Test.js
@@ -0,0 +1,162 @@
+import React, {useState} from 'react';
+import logo from './temp_logo.png'
+import {json} from "react-router-dom";
+
+
+export default function Login() {
+    const [loginInfo, setLoginInfo] = useState({
+        id: "",
+        pw: "",
+    });
+
+    const otherInfo = {
+        name: "",
+        age: 0,
+    };
+
+
+    const handleLogin = async () => {
+        const response = await fetch("/user/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Accept": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify(loginInfo),
+        });
+        const result = await response.json();
+        if (result.code === "200") {
+            // 로그인 성공 처리
+            window.sessionStorage.setItem("observe", result.body);
+            window.location.href = "/main";
+        } else {
+            // 로그인 실패 처리
+            alert(result.body);
+            // window.location.href = "/";
+        }
+    };
+
+    const handleGoogleLogin = async () => {
+        const response = await fetch("/google/login", {
+            mode: "no-cors",
+        }); // fetch 호출이 아예 안됨
+        console.log(response.json());
+        alert(response.json());
+        window.location.href = response.json();
+    };
+
+
+    const [observeToken] = useState({
+        observe : sessionStorage.getItem("observe")
+    });
+
+    const [noteWrite, setNoteWrite] = useState({
+        categoryId : "",
+        type : "",
+        status : "",
+        startTime : "",
+        endTime : "",
+        title : "",
+        contents : "",
+
+    });
+
+    const handleNoteWrite = async () => {
+        const response = await fetch("/notes/write", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Accept": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify({...noteWrite, ...observeToken}),
+        });
+        const result = await response.json();
+        if (result.code === "200") {
+            // 글 작성 성공 성공 처리
+            window.location.href = "/main";
+        } else {
+            // 로그인 실패 처리
+            alert(result.body);
+            // window.location.href = "/";
+        }
+    };
+
+
+    const handleLogout = async () => {
+        const response = await fetch("/user/allLogout", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Accept": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify(observeToken),
+        });
+        const result = await response.json();
+        if (result.code === "200") {
+            // 전체 로그아웃 성공 처리
+            window.sessionStorage.setItem("observe", null);
+            window.location.href = "";
+        } else {
+            // 전체 로그아웃 실패 처리
+            alert(result.body);
+        }
+    };
+
+    const handleUnlink = async () => {
+        const response = await fetch("/notes/unlink", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Accept": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify(observeToken),
+        });
+        const result = await response.json();
+        if (result.code === "200") {
+            // 연동 해제 성공시 처리
+            window.location.href = "/main";
+        } else {
+            // 연동해제 실패 처리
+            alert(result.body);
+        }
+    };
+
+    return (
+        <div className={"loginall"}>
+            <div className={"login-main"}>
+                <div className={"Logoclass"}>
+                    <img src={logo}/>
+                </div>
+                <div className={"logininput"}>
+                    <div className={"loginForm1"}>
+                        <input
+                            type="text"
+                            className="inputtext"
+                            name="id"
+                            placeholder="email"
+                            value={loginInfo.id}
+                            onChange={(e) => setLoginInfo({...loginInfo, id: e.target.value})}
+                        />
+                        <input
+                            type="password"
+                            className="inputtext"
+                            name="pw"
+                            placeholder="password"
+                            value={loginInfo.pw}
+                            onChange={(e) => setLoginInfo({...loginInfo, pw: e.target.value})}
+                        />
+                        <div className={"autologin"}>
+                            <input type={"checkbox"}/>로그인유지
+                        </div>
+                        <button className={"btn"} type={"submit"} onClick={handleLogin}>로그인</button>
+                    </div>
+                    <a href={"/create"}>
+                        <button className={"btn"}>회원가입</button>
+                    </a>
+                    <hr/>
+                    <button className={"btn"} onClick={handleGoogleLogin}>google계정으로 로그인</button>
+                </div>
+            </div>
+        </div>
+    )
+}
